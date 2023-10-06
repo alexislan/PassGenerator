8e9d3e5045e245cf33ce6c4e5156e37bf565434b
alexislan <alexislanchiotti13@gmail.com>
2023-10-06 17:01:03 -0300

    uso del useId

--- src/components/Checks.jsx
+++ src/components/Checks.jsx
@@ -1,10 +1,13 @@
+import { useId } from "react"
 import "./Checks.css"
 
 export const Checks = ({label, t, val, onC}) => {
+const labelInput = useId();
     return (
         <div className={label == "Cantidad de Caracteres:" ? "Largo" : "Check"}>
-          <label>{label}</label>
+          <label htmlFor={labelInput}>{label}</label>
           <input
+            id={labelInput}
             type= {t}
             value={val}
             onChange={onC}
